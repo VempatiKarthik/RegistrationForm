47d5b9b705bbadb4a4a297b9b92dd1faba68a49d
Selam Bhanu Prakash <bhanu4480@gmail.com>
2023-08-01 17:41:23 +0530

    Fixed the index.js file in RegistarionForm folder but not in src folder file

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import App from './App';
 
 // const root= ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(
-//   <React.StrictMode>
-//     <App/>
-//   </React.StrictMode>
-// );
-
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode >, document.getElementById('root'),
-)
+    <App/>
+  </React.StrictMode>
+);
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode >, document.getElementById('root'),
+// )
 
 
 
